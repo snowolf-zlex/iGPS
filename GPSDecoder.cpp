ecf385a0da34928ef7dd5af46fdaa63adbb91be6
snowolf-zlex <zlex.dongliang@gmail.com>
2013-07-27 21:24:03 +0800

    Update GPSDecoder.cpp

--- GPSDecoder.cpp
+++ GPSDecoder.cpp
@@ -20,32 +20,31 @@ void GPSDecoder::decode(String sub) {
     }
 }
 
-
-
-String GPSDecoder::utcTime() {
-    return _utcTime;
-}
-
-
-String GPSDecoder::utcDate() {
-    return _utcDate;
-}
-
 bool GPSDecoder::isValid() {
     return _validStatus;
 }
 
 
+String GPSDecoder::utcTime() {
+    return _utcTimeString;
+}
+
+
+String GPSDecoder::utcDate() {
+    return _utcDateString;
+}
+
+
 String GPSDecoder::longitude() {
-    return _longitude;
+    return _longitudeString;
 }
 
 String GPSDecoder::latitude() {
-    return _latitude;
+    return _latitudeString;
 }
 
 String GPSDecoder::altitude() {
-    return _altitude;
+    return _altitudeString;
 }
 
 
@@ -82,81 +81,73 @@ double GPSDecoder::toDouble(String str){
 
 // process GPRMC
 void GPSDecoder::processGPRMC(String sub){
-    String time = splitString(sub,',',1);
+    String valid = split(sub,',',2);
+    _validStatus = (valid.equals(VALID) ? true : false);
+    if(!_validStatus) {
+        return;
+    }
+    
+    String time = split(sub,',',1);
     String hour = time.substring(0, 2);
     String minute = time.substring(2, 4);
     String second = time.substring(4, 6);
     _hour = hour.toInt();
     _minute = minute.toInt();
     _second = second.toInt();
-    _utcTime = hour + ':' + minute + ':' + second;
     
-//    // Serial.println(time);
-//    
-//    int localHour = ((hour + LOCAL_AREA) % 24); // 8
-//    String localHourStr = String(100 + localHour); // 108
-//    localHourStr = localHourStr.substring(localHourStr.indexOf('1')+1, 3); // 08
-//    
-//    localTime = time;
-//    localTime.replace(localTime.substring(0,2),localHourStr);
-//    
-//    // Serial.println(localTime);
-//    
-    String date = splitString(sub,',',9);
+    _utcTimeString = hour + ':' + minute + ':' + second;
+
+    
+    String date = split(sub,',',9);
     String day = date.substring(0,2);
     String month = date.substring(2,4);
     String year = date.substring(4,6);
     _day = day.toInt();
     _month = month.toInt();
     _year = year.toInt();
-    _utcDate = day + '/' + month + '/' + year;
-//
-//    // Serial.println(date);
-//    
-    String valid = splitString(sub,',',2);
-    _validStatus = (valid.equals(VALID) ? true : false);
-//
-//    if(validStatus) {
-        // Latitude
-        String lat = splitString(sub,',',3);
-        String degreeOfLat = lat.substring(0,2);
-        String minuteOfLat = lat.substring(2,4);
-        String secondOfLat = lat.substring(5,8);
-        _degreeOfLatitude = degreeOfLat.toInt();
-        _minuteOfLatitude = minuteOfLat.toInt();
-        _secondOfLatitude = secondOfLat.toInt()*60/1000;
-        
-        _northingIndicator = splitString(sub,',',4).charAt(0);
-        _latitude = degreeOfLat + CHAR_DEGREE + minuteOfLat + CHAR_MINUTE + secondOfLat + CHAR_SECOND + _northingIndicator;
-//        // Serial.println(latString);
-//        
-        // Longitude
-        String lon = splitString(sub,',',5);
-        String degreeOfLon = lon.substring(0,3);
-        String minuteOfLon = lon.substring(3,5);
-        String secondOfLon = lon.substring(6,9);
-        _degreeOfLongitude = degreeOfLon.toInt();
-        _minuteOfLongitude = minuteOfLon.toInt();
-        _secondOfLongitude = secondOfLon.toInt()*60/1000;
-//
-        _eastingIndicator = splitString(sub,',',6).charAt(0);
-        _longitude = degreeOfLon + CHAR_DEGREE + minuteOfLon + CHAR_MINUTE + secondOfLon + CHAR_SECOND + _eastingIndicator;
-//        // Serial.println(lonString);
-//    }
+ 
+    _utcDateString = day + '/' + month + '/' + year;
+  
+    // Latitude
+    String lat = split(sub,',',3);
+    String degreeOfLat = lat.substring(0,2);
+    String minuteOfLat = lat.substring(2,4);
+    String secondOfLat = lat.substring(5,8);
+    _degreeOfLatitude = degreeOfLat.toInt();
+    _minuteOfLatitude = minuteOfLat.toInt();
+    _secondOfLatitude = secondOfLat.toInt() * 60 / 1000;
+    _northingIndicator = split(sub,',',4).charAt(0);
+    _latitude = toDouble(lat);
+
+    _latitudeString = degreeOfLat + CHAR_DEGREE + minuteOfLat + CHAR_MINUTE + secondOfLat + CHAR_SECOND + ' ' + _northingIndicator;
+    
+    // Longitude
+    String lon = split(sub,',',5);
+    String degreeOfLon = lon.substring(0,3);
+    String minuteOfLon = lon.substring(3,5);
+    String secondOfLon = lon.substring(6,9);
+    _degreeOfLongitude = degreeOfLon.toInt();
+    _minuteOfLongitude = minuteOfLon.toInt();
+    _secondOfLongitude = secondOfLon.toInt() * 60 / 1000;
+    _eastingIndicator = split(sub,',',6).charAt(0);
+    _longitude = toDouble(lon);
+    
+    _longitudeString = degreeOfLon + CHAR_DEGREE + minuteOfLon + CHAR_MINUTE + secondOfLon + CHAR_SECOND + ' ' + _eastingIndicator;
+
 }
 
 // process GPGGA
 void GPSDecoder::processGPGGA(String sub){
-    boolean ggaStatus = splitString(sub,',',6).toInt();
+    boolean ggaStatus = split(sub,',',6).toInt();
     if(ggaStatus){
-        _altitude = splitString(sub,',',9);
-        _altitude += splitString(sub,',',10);
-        //  Serial.println(altString);
+        String altitude = split(sub,',',9);
+        _altitude = toDouble(altitude);
+        _altitudeString = altitude + split(sub,',',10);
     }
 }
 
 // Split String
-String GPSDecoder::splitString(String s, char parser,int index) {
+String GPSDecoder::split(String s, char parser,int index) {
     String rs = '\0';
     int parserIndex = index;
     int parserCnt = 0;
